098dd8a7c2d2409853064574f5f7d529d642bd53
Rodger Xiang <rodxiang2@gmail.com>
2024-03-14 19:11:17 +1000

    added wandb logs

--- cleanrl/dqn.py
+++ cleanrl/dqn.py
@@ -187,7 +187,8 @@ poetry run pip install "stable_baselines3==2.0.0a1"
                     print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
                     writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
                     writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
-
+                if args.track:
+                    wandb.log({"global_step": global_step, "episodic_return": info["episode"]["r"], "episodic_length": info["episode"]["l"]})
         # TRY NOT TO MODIFY: save data to reply buffer; handle `final_observation`
         real_next_obs = next_obs.copy()
         for idx, trunc in enumerate(truncations):
@@ -213,7 +214,8 @@ poetry run pip install "stable_baselines3==2.0.0a1"
                     writer.add_scalar("losses/q_values", old_val.mean().item(), global_step)
                     print("SPS:", int(global_step / (time.time() - start_time)))
                     writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
-
+                    if args.track:
+                        wandb.log({"loss": loss, "q_values": old_val.mean().item(), "SPS": int(global_step / (time.time() - start_time))})
                 # optimize the model
                 optimizer.zero_grad()
                 loss.backward()
@@ -244,13 +246,8 @@ poetry run pip install "stable_baselines3==2.0.0a1"
         )
         for idx, episodic_return in enumerate(episodic_returns):
             writer.add_scalar("eval/episodic_return", episodic_return, idx)
-
-        if args.upload_model:
-            from cleanrl_utils.huggingface import push_to_hub
-
-            repo_name = f"{args.env_id}-{args.exp_name}-seed{args.seed}"
-            repo_id = f"{args.hf_entity}/{repo_name}" if args.hf_entity else repo_name
-            push_to_hub(args, episodic_returns, repo_id, "DQN", f"runs/{run_name}", f"videos/{run_name}-eval")
+            if args.track:
+                wandb.log({"eval_episodic_return": episodic_return})
 
     envs.close()
     writer.close()
